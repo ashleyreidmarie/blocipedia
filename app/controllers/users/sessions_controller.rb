9df8846b18df475d0cf369e38169fc13d298edb7
Ashley <ashleyreidmarie@gmail.com>
2016-06-22 02:38:11 +0000

    Create Wiki Model

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -17,10 +17,11 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email])
   end
+  
 end
