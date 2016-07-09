ea086559ad643867e8c856118874373bedcd5bf2
Ashley <ashleyreidmarie@gmail.com>
2016-07-09 01:28:29 +0000

    Break login_user method for troubleshooting

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,10 +1,9 @@
 module ControllerMacros
-  def login_user()
-    before(:each) do
-      user = FactoryGirl.create(:user)
+  def login_user(user = FactoryGirl.create(:user))
+    # before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
       user.confirm
       sign_in user
-    end
+    # end
   end
 end
