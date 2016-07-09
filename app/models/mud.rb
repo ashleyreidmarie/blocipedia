dbe0a11b39d4c9a6f8448543f79c1cbeac5dcad1
Adam Louis <adamlouis@gmail.com>
2016-07-08 19:10:35 -0700

    fix indentation in mud model

--- app/models/mud.rb
+++ app/models/mud.rb
@@ -1,19 +1,19 @@
 class Mud < ActiveRecord::Base
-    has_many :wikis, dependent: :destroy
+  has_many :wikis, dependent: :destroy
+  
+  before_save { self.name[0] = self.name[0].upcase if self.name }
+  
+  #Mud scopes
+  default_scope { order('name') } 
+  scope :approved, ->{where(approved: true)}
+  scope :unapproved, ->{where(approved: false)}
+  
+  #Mud validations
+  validates :name, 
+    length: { minimum: 2, maximum: 100 }, 
+    presence: true, 
+    uniqueness: true, 
+    format: {message: 'cannot contain special characters', without: /[@\\\/+*?\[^\]$(){}=!<>|:]/}
     
-    before_save { self.name[0] = self.name[0].upcase if self.name }
-    
-    #Mud scopes
-    default_scope { order('name') } 
-    scope :approved, ->{where(approved: true)}
-    scope :unapproved, ->{where(approved: false)}
-    
-    #Mud validations
-    validates :name, 
-      length: { minimum: 2, maximum: 100 }, 
-      presence: true, 
-      uniqueness: true, 
-      format: {message: 'cannot contain special characters', without: /[@\\\/+*?\[^\]$(){}=!<>|:]/}
-      
-    validates :url, presence: true, uniqueness: true, :url => true
+  validates :url, presence: true, uniqueness: true, :url => true
 end
