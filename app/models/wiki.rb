0a2289917335804a241fcedef10313b1b9016edd
Ashley <ashleyreidmarie@gmail.com>
2016-07-25 01:26:47 +0000

    Finish Wiki and Page validations

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -3,7 +3,14 @@ class Wiki < ActiveRecord::Base
   belongs_to :mud
   has_many :pages, dependent: :destroy
   
-  validates :name, length: { minimum: 3 }, presence: true, uniqueness: true
+  before_save { self.name[0] = self.name[0].upcase if self.name}
+  
+  validates :name, 
+    length: { minimum: 3 }, 
+    presence: true, 
+    uniqueness: true,
+    format: {message: 'Cannot contain those special characters', without: /[@\\\/+*?\[^\]$(){}=!<>|]/}
+    
   validates :description, length: {minimum: 5}, presence: true
   validates :mud, presence: true
   validates :user, presence: true
